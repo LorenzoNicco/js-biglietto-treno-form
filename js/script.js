2933489bacc702face0b673e9a71f38af4eab518
LorenzoNicco <lorenzoniccolai91@gmail.com>
2022-12-20 14:53:38 +0100

    Inizializzazione progetto.

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/*
+    1. Inizio
+    2. Chiedere all'utente quanti chilometri vuole percorrere
+    3. Ciedere all'utente quanti anni ha
+    4. Calcolare il prezzo del biglietto
+        4.1 0.21 euro al chilometro
+        4.2 Sconto del 20% per under 18
+        4.3 Sconto del 40% per over 65
+
+    MILESTONE 1
+        Due input e un bottone non stilizzati con label sopra. Output in console
+
+    MILESTONE 2
+        1. L'utente può inserire i dati e visualizzare il calcolo finale con il prezzo.
+        2. Recap dei dati e output del prezzo finale stampati in pagina (il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo).
+*/
