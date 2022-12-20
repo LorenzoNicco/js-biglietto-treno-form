4d29c9ace3dd273161ebaf81044a73cea0780cf0
LorenzoNicco <lorenzoniccolai91@gmail.com>
2022-12-20 18:56:04 +0100

    Add milestone 2

--- js/script.js
+++ js/script.js
@@ -16,6 +16,67 @@
 */
 
 // MILESTONE 1
+// // richiesta chilometri
+// let km = document.getElementById('distance');
+
+// console.log('Debug ok');
+
+
+// // richiesta età
+// let age = document.getElementById('years');
+
+// console.log('Debug ok');
+
+
+// // generazione output
+// const submitButton = document.getElementById('submit-button');
+
+// submitButton.addEventListener('click', 
+//     function () {
+//         km = parseInt(km.value);
+
+//         console.log('km', km, typeof km, isNaN(km));
+
+//         age = parseInt(age.value);
+
+//         console.log('age', age, typeof age, isNaN(age));
+
+//         console.log('I dati sono stati inviati');
+
+//         const ticketPrice = (km * 0.21).toFixed(2);
+
+//         console.log('ticketPrice', ticketPrice, typeof ticketPrice, isNaN(ticketPrice));
+//         console.log('Debug ok');
+
+//         if (age < 18) {
+//             const under18Ticket = (ticketPrice * 0.8).toFixed(2);
+
+//             console.log('under18Ticket', under18Ticket, typeof under18Ticket, isNaN(under18Ticket));
+//             console.log('Debug ok');
+
+//             console.log('Il costo del tuo biglietto è di euro ' + under18Ticket);
+//         }
+//         else if (age >= 65) {
+//             const over65Ticket = (ticketPrice * 0.6).toFixed(2);
+
+//             console.log('over65Ticket', over65Ticket, typeof over65Ticket, isNaN(over65Ticket));
+//             console.log('Debug ok');
+
+//             console.log('Il costo del tuo biglietto è di euro ' + over65Ticket);
+//         }
+//         else {
+//             console.log('Il costo del tuo biglietto è di euro ' + ticketPrice);
+//         }
+//     }
+// );
+
+// MILESTONE 2
+// richiesta nome
+const identity = document.getElementById('username');
+
+
+console.log('Debug ok');
+
 // richiesta chilometri
 let km = document.getElementById('distance');
 
@@ -33,13 +94,15 @@ const submitButton = document.getElementById('submit-button');
 
 submitButton.addEventListener('click', 
     function () {
+        document.getElementById('ticket-result').style.display = 'block';
+
+        document.getElementById('fill-name').innerHTML = identity.value;
+
+        console.log('identity', identity, typeof identity);
+
         km = parseInt(km.value);
 
         console.log('km', km, typeof km, isNaN(km));
-
-        age = parseInt(age.value);
-
-        console.log('age', age, typeof age, isNaN(age));
 
         console.log('I dati sono stati inviati');
 
@@ -48,16 +111,32 @@ submitButton.addEventListener('click',
         console.log('ticketPrice', ticketPrice, typeof ticketPrice, isNaN(ticketPrice));
         console.log('Debug ok');
 
-        if (age < 18) {
+        if (age.value == 'minorenne') {
             const under18Ticket = (ticketPrice * 0.8).toFixed(2);
+
+            document.getElementById('offer').innerHTML = 'Biglietto young';
+
+            document.getElementById('carrozza').innerHTML = Math.floor(Math.random() * 10) + 1;
+
+            document.getElementById('code').innerHTML = Math.floor(Math.random() * (9999 - 9000 + 1) ) + 9000;
+
+            document.getElementById('price').innerHTML = 'euro ' + under18Ticket;
 
             console.log('under18Ticket', under18Ticket, typeof under18Ticket, isNaN(under18Ticket));
             console.log('Debug ok');
 
             console.log('Il costo del tuo biglietto è di euro ' + under18Ticket);
         }
-        else if (age >= 65) {
+        else if (age.value == 'silver') {
             const over65Ticket = (ticketPrice * 0.6).toFixed(2);
+
+            document.getElementById('offer').innerHTML = 'Biglietto Silver';
+
+            document.getElementById('carrozza').innerHTML = Math.floor(Math.random() * 10) + 1;
+
+            document.getElementById('code').innerHTML = Math.floor(Math.random() * (9999 - 9000 + 1) ) + 9000;
+
+            document.getElementById('price').innerHTML = 'euro ' + over65Ticket;
 
             console.log('over65Ticket', over65Ticket, typeof over65Ticket, isNaN(over65Ticket));
             console.log('Debug ok');
@@ -65,7 +144,23 @@ submitButton.addEventListener('click',
             console.log('Il costo del tuo biglietto è di euro ' + over65Ticket);
         }
         else {
+            document.getElementById('offer').innerHTML = 'Biglietto Standard';
+
+            document.getElementById('carrozza').innerHTML = Math.floor(Math.random() * 10) + 1;
+
+            document.getElementById('code').innerHTML = Math.floor(Math.random() * (9999 - 9000 + 1) ) + 9000;
+
+            document.getElementById('price').innerHTML = 'euro ' + ticketPrice;
+
             console.log('Il costo del tuo biglietto è di euro ' + ticketPrice);
         }
+    }
+);
+
+const cancelButton = document.getElementById('cancel-button');
+
+cancelButton.addEventListener('click', 
+    function () {
+        window.location.reload();
     }
 );
