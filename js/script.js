a99d6d782c17c162d8ab2d7db4bf95fa809e78f7
LorenzoNicco <lorenzoniccolai91@gmail.com>
2022-12-20 16:52:14 +0100

    Update function milestone 1 e init milestone 2 css

--- js/script.js
+++ js/script.js
@@ -18,50 +18,54 @@
 // MILESTONE 1
 // richiesta chilometri
 let km = document.getElementById('distance');
-km = parseInt(km);
 
-console.log('km', km, typeof km, isNaN(km));
 console.log('Debug ok');
+
 
 // richiesta età
 let age = document.getElementById('years');
-age = parseInt(age);
 
-console.log('age', age, typeof age, isNaN(age));
 console.log('Debug ok');
 
-// prezzo biglietto
-const ticketPrice = (km * 0.21).toFixed(2);
-
-console.log('ticketPrice', ticketPrice, typeof ticketPrice, isNaN(ticketPrice));
-console.log('Debug ok');
-
-const under18Ticket = (ticketPrice * 0.8).toFixed(2);
-
-console.log('under18Ticket', under18Ticket, typeof under18Ticket, isNaN(under18Ticket));
-console.log('Debug ok');
-
-
-const over65Ticket = (ticketPrice * 0.6).toFixed(2);
-
-console.log('over65Ticket', over65Ticket, typeof over65Ticket, isNaN(over65Ticket));
-console.log('Debug ok');
 
 // generazione output
 const submitButton = document.getElementById('submit-button');
 
 submitButton.addEventListener('click', 
     function () {
+        km = parseInt(km.value);
+
+        console.log('km', km, typeof km, isNaN(km));
+
+        age = parseInt(age.value);
+
+        console.log('age', age, typeof age, isNaN(age));
+
         console.log('I dati sono stati inviati');
-        
+
+        const ticketPrice = (km * 0.21).toFixed(2);
+
+        console.log('ticketPrice', ticketPrice, typeof ticketPrice, isNaN(ticketPrice));
+        console.log('Debug ok');
+
         if (age < 18) {
-            console.log('Il costo del tuo biglietto è di euro' + under18Ticket);
+            const under18Ticket = (ticketPrice * 0.8).toFixed(2);
+
+            console.log('under18Ticket', under18Ticket, typeof under18Ticket, isNaN(under18Ticket));
+            console.log('Debug ok');
+
+            console.log('Il costo del tuo biglietto è di euro ' + under18Ticket);
         }
         else if (age >= 65) {
-            console.log('Il costo del tuo biglietto è di euro' + over65Ticket);
+            const over65Ticket = (ticketPrice * 0.6).toFixed(2);
+
+            console.log('over65Ticket', over65Ticket, typeof over65Ticket, isNaN(over65Ticket));
+            console.log('Debug ok');
+
+            console.log('Il costo del tuo biglietto è di euro ' + over65Ticket);
         }
         else {
-            console.log('Il costo del tuo biglietto è di euro' + ticketPrice);
+            console.log('Il costo del tuo biglietto è di euro ' + ticketPrice);
         }
     }
 );
